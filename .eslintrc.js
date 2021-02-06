421133159365ffe9beff93da9cf7343ff5bdccec
wesley guerra <guerra_ssa@hotmail.com>
2021-02-06 17:01:24 -0300

    eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   env: {
     browser: true,
+    // es6: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomic: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -14,9 +17,13 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 0,
   },
 };
