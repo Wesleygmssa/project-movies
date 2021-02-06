7f0ab307de5e11f6bb755293267ac32c61f5ae31
wesley guerra <guerra_ssa@hotmail.com>
2021-02-06 16:48:06 -0300

    cleaning design ok

--- src/pages/Aluno/index.js
+++
@@ -1,9 +0,0 @@
-import { Container } from '../../styles/Global';
-
-export default function Aluno() {
-  return (
-    <Container>
-      <h1>Fotos</h1>
-    </Container>
-  );
-}
--- src/pages/Login/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Title = styled.h1`
-  background: red;
-`;
-
-export const Paragrafo = styled.p``;
